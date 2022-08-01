08bbe6dcc27a8cfa23674c9288a3420554f5fea6
mooncy0809 <mooncy0809@naver.com>
2022-08-01 17:50:24 +0900

    g마켓 서브카테고리 수집

--- mission_crawling3/gmarket_crawler.py
+++ mission_crawling3/gmarket_crawler.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options # Chrome WebDriver의 옵션을 설정하는 모듈 Import
 from datetime import datetime
-from bs4 import BeautifulSoup
 from pprint import pprint
 import time
 
@@ -24,47 +23,44 @@ driver.get(url)
 def scroll_down_to_end():
     check_body_height = 0
     while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") # 맨끝까지
-        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)") #맨끝까지
+        time.sleep(0.5)
         body_height = driver.execute_script("return document.body.clientHeight;")
         # 스크롤이 사이트 마지막에 다다르면 while -> break
         if check_body_height == body_height:
             break
         check_body_height = body_height
-        time.sleep(1)
+        time.sleep(0.5)
 
 def scroll_up_to_category():
     driver.execute_script("window.scrollTo(0, -document.body.scrollHeight)")
 
-def crawler(cate,sub_cate):
+def crawler(cate):
     market_name = '11번가'
     crawling_date = datetime.today().strftime("%Y-%m-%d")
-    req = driver.page_source
-    soup = BeautifulSoup(req, 'html.parser')
-    elements = soup.select('#bestPrdList > div > ul > li')
-    results = []
+    elements = driver.find_elements_by_xpath('//*[@id="bestPrdList"]/div/ul/li')
+    results =[]
     for el in elements:
         temp_dict = dict()
+        ranking = el.find_element_by_xpath('div/a/span')
+        ranking = ranking.text
+        name = el.find_element_by_xpath('div/a/div[2]/p')
+        name = name.text
+        image = el.find_element_by_xpath('div/a/div[1]/img')
+        image = image.get_attribute('src')
+        sales_price = el.find_element_by_xpath('div/a/div[2]/div[1]/span/strong')
+        sales_price = sales_price.text
+        sales_price = int(sales_price.replace(',', ''))
         cate = cate
-        sub_cate = sub_cate
-        ranking = el.find('span', attrs={"class": "best"}).text
-        name = el.find('div', attrs={"class": "pname"}).find('p').text
-        image = el.find('div', attrs={"class": "img_plot"}).find('img')['src']
         try:
-            sales_price = el.find('strong', attrs={"class": "sale_price"}).text
-            sales_price = int(sales_price.replace(',', ''))
-        except:
-            sales_price = 0 # 무료상품
-
-        try:
-            rate = el.find('span', attrs={"class": "sale"}).text.strip('%')
+            rate = el.find_element_by_xpath('div/a/div[2]/div[1]/span[1]')
+            rate = rate.text.strip('%')
             rate = float(rate)
         except:
-            rate = 0 # 할인하지 않는 상품
+            rate = 0
         temp_dict['crawling_date'] = crawling_date
         temp_dict['market_name'] = market_name
         temp_dict['category_name'] = cate
-        temp_dict['sub_category_name'] = sub_cate
         temp_dict['ranking'] = ranking
         temp_dict['product_name'] = name
         temp_dict['sales_price'] = sales_price
@@ -72,37 +68,21 @@ def crawler(cate,sub_cate):
         temp_dict['sales_rate'] = '%.2f' % rate
         results.append(temp_dict.copy())
 
-    pprint(results)
     return results
 
+
 def execute():
-    category_lists = driver.find_elements(By.XPATH, "//div[@class='best_category_box']/ul/li/button")
+    elements = driver.find_elements(By.XPATH, "//div[@class='best_category_box']/ul/li/button")
     all_result = []
-    for button_index in range(len(category_lists)):
-        category = driver.find_element_by_xpath('//*[@id="metaCtgrLi' + str(button_index) + '"]/button') # 카테고리 find
-        cate = category.text
-        sub_cate = "전체"
-        category.click() # 카테고리 클릭하여 페이지 이동
-        time.sleep(1)
+    for button_index in range(len(elements)):
+        element = driver.find_element_by_xpath('//*[@id="metaCtgrLi' + str(button_index) + '"]/button')
+        cate = element.text
+        element.click()
         scroll_down_to_end()
-        result_list = crawler(cate,sub_cate)
+        result_list = crawler(cate)
+        # print(f"resulst_list : {result_list}") # 카테고리별 리스트
         all_result.extend(result_list)
         scroll_up_to_category()
-        if button_index == 0:
-            pass
-        else:
-            sub_category_lists = driver.find_elements_by_xpath('//*[@id="metaCtgrLi' + str(button_index) + '"]/div/ul/li/a')
-            for a_index in range(1,len(sub_category_lists)+1):
-                sub_category = driver.find_element_by_xpath('//*[@id="metaCtgrLi' + str(button_index) + '"]/div/ul/li['+str(a_index)+']/a') # 서브 카테고리
-                sub_cate = sub_category.text
-                sub_category.click() # 서브 카테고리 클릭하여 페이지 이동
-                time.sleep(1)
-                scroll_down_to_end()
-                result_list = crawler(cate,sub_cate)
-                all_result.extend(result_list)
-                scroll_up_to_category()
-
-        # pprint(f"resulst_list : {result_list}")  # 카테고리별 리스트
     return all_result
 
 all_results = execute()
